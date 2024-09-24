1a42f66c2f5cf49415d5eefaf444cd928610bbf9
Андрей Лебедев <andreylebedev@MacBook-Air-Andrej.local>
2024-09-25 00:09:57 +0300

    first commit

---
+++ example_runner_EM_classifier copy.py
@@ -0,0 +1,16 @@
+import pickle
+import sys
+import warnings
+
+warnings.filterwarnings("ignore")
+
+from maps import ElasticMapClassifier
+
+if __name__ == "__main__":
+    path_to_data = sys.argv[1]
+    with open(path_to_data, "rb") as f:
+        data = pickle.load(f)
+    em = ElasticMapClassifier(map_size=(10, 10))
+    em.load_data(X=data["features"], y=data["target"], test_size=0.2)
+    em.train()
+    em.evaluate()
